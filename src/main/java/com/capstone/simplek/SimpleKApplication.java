bd0df7044d9175a9f6d5d905446e855bcc5162e1
AaronBoehle <boehle.aaron@gmail.com>
2019-03-12 11:11:58 -0500

    init commit

---
+++ src/main/java/com/capstone/simplek/SimpleKApplication.java
@@ -0,0 +1,13 @@
+package com.capstone.simplek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimpleKApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SimpleKApplication.class, args);
+    }
+
+}
