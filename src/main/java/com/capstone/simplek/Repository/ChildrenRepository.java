b707ce81ea8f7ed34635d2ff4819fff39c0b3819
AaronBoehle <boehle.aaron@gmail.com>
2019-03-14 15:59:57 -0500

    added templates for QueryController and mapped GET requests. Needs formatting and logic.

--- src/main/java/com/capstone/simplek/Repository/ChildrenRepository.java
+++ src/main/java/com/capstone/simplek/Repository/ChildrenRepository.java
@@ -6,6 +6,5 @@ import org.springframework.stereotype.Repository;
 
 // comes with built in findAll, findOne, save, and delete methods
 // JpaRepository implements the same functionality as the CrudRepository
-@Repository
 public interface ChildrenRepository extends JpaRepository<Children, String> {
 }
--- src/main/java/com/capstone/simplek/controllers/QueryController.java
+++ src/main/java/com/capstone/simplek/controllers/QueryController.java
@@ -1,28 +1,40 @@
 package com.capstone.simplek.controllers;
 
-import com.capstone.simplek.Model.Children;
 import com.capstone.simplek.Repository.ChildrenRepository;
 import com.capstone.simplek.Repository.DistrictRepository;
 import com.capstone.simplek.Repository.SchoolRepository;
+import com.capstone.simplek.Repository.ServiceRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class QueryController {
 
-//    collection of repos referring to database for queries and sub queries
+//    collection of repos referring to our database
+    @Autowired
+    private ChildrenRepository childrenDao;
+    @Autowired
+    private DistrictRepository districtDao;
+    @Autowired
+    private SchoolRepository schoolDao;
+    @Autowired
+    private ServiceRepository serviceDao;
 
 
-//    private final ChildrenRepository childrenDao;
-//    public QueryController(ChildrenRepository childrenDao) {this.childrenDao = childrenDao;}
-//
-//    private final DistrictRepository districtDao;
-//    public QueryController(DistrictRepository districtDao) {this.districtDao = districtDao;}
+//     requests that interact with our Dao Factory
+    @GetMapping("/query/district")
+    public String districtFilter (){
+        return "query/district";
+    }
 
-    @GetMapping("/query")
-    public String stepOne (){
-        return "query";
+    @GetMapping("/query/transportation")
+    public String transportation (){
+        return "query/transportation";
+    }
+
+    @GetMapping("/query/disabilities")
+    public String disabilities (){
+        return "query/disabilities";
     }
 }
