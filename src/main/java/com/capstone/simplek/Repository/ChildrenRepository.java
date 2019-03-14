b3bbed82144e1c935be9e506974e332111276681
AaronBoehle <boehle.aaron@gmail.com>
2019-03-14 14:05:21 -0500

    prepping to store attributes in session

--- src/main/java/com/capstone/simplek/Repository/ChildrenRepository.java
+++ src/main/java/com/capstone/simplek/Repository/ChildrenRepository.java
@@ -2,8 +2,10 @@ package com.capstone.simplek.Repository;
 
 import com.capstone.simplek.Model.Children;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 // comes with built in findAll, findOne, save, and delete methods
 // JpaRepository implements the same functionality as the CrudRepository
+@Repository
 public interface ChildrenRepository extends JpaRepository<Children, String> {
 }
--- src/main/java/com/capstone/simplek/controllers/QueryController.java
+++ src/main/java/com/capstone/simplek/controllers/QueryController.java
@@ -1,8 +1,28 @@
 package com.capstone.simplek.controllers;
 
+import com.capstone.simplek.Model.Children;
+import com.capstone.simplek.Repository.ChildrenRepository;
+import com.capstone.simplek.Repository.DistrictRepository;
+import com.capstone.simplek.Repository.SchoolRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class QueryController {
+
+//    collection of repos referring to database for queries and sub queries
+
+
+//    private final ChildrenRepository childrenDao;
+//    public QueryController(ChildrenRepository childrenDao) {this.childrenDao = childrenDao;}
+//
+//    private final DistrictRepository districtDao;
+//    public QueryController(DistrictRepository districtDao) {this.districtDao = districtDao;}
+
+    @GetMapping("/query")
+    public String stepOne (){
+        return "query";
+    }
 }
