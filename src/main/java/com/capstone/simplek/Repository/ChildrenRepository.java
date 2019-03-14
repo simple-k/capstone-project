da18fdc260273ae5c54af5cecda8d43ae50e907c
AaronBoehle <boehle.aaron@gmail.com>
2019-03-14 10:35:25 -0500

    css update

--- src/main/java/com/capstone/simplek/Repository/ChildrenRepository.java
+++ src/main/java/com/capstone/simplek/Repository/ChildrenRepository.java
@@ -1,11 +1,9 @@
 package com.capstone.simplek.Repository;
-import com.capstone.simplek.Model.User;
+
+import com.capstone.simplek.Model.Children;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 // comes with built in findAll, findOne, save, and delete methods
 // JpaRepository implements the same functionality as the CrudRepository
-public interface UserRepository extends JpaRepository<User, Long> {
-
-    User findByUsername(String username);
-
-}// interface
+public interface ChildrenRepository extends JpaRepository<Children, String> {
+}
