93ea783760c1d8148859659fd6e944b520a113d8
Bryan Matta <bryan.m.matta@gmail.com>
2019-03-15 16:01:25 -0500

    added map

---
+++ src/main/java/com/capstone/simplek/controllers/MapsController.java
@@ -0,0 +1,12 @@
+package com.capstone.simplek.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MapsController {
+    @GetMapping("/test")
+    public String transportation (){
+        return "school-results";
+    }
+}
