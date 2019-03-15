c02246f151f188d26a86279e9aad9a394a2d2e1a
Ron Soeum <ronsoeum@gmail.com>
2019-03-15 11:11:54 -0500

    added login feature with security authentication

--- src/main/java/com/capstone/simplek/controllers/AuthenticationController.java
+++ src/main/java/com/capstone/simplek/controllers/AuthenticationController.java
@@ -3,7 +3,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
-public class LoginController {
+public class AuthenticationController {
     @GetMapping("/login")
     public String showLoginForm() {
         return "user/login";
--- src/main/java/com/capstone/simplek/controllers/UserController.java
+++ src/main/java/com/capstone/simplek/controllers/UserController.java
@@ -2,6 +2,7 @@ package com.capstone.simplek.controllers;
 import com.capstone.simplek.Model.User;
 import com.capstone.simplek.Repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,18 +12,26 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class UserController {
 
-    @Autowired
     private UserRepository userDao;
+    private PasswordEncoder passwordEncoder;
+
+    public UserController(UserRepository userDao, PasswordEncoder passwordEncoder) {
+        this.userDao = userDao;
+        this.passwordEncoder = passwordEncoder;
+    }
 
     @GetMapping("/register")
-    public String registerForm(Model model){
+    public String showRegisterForm(Model model){
         model.addAttribute("user", new User());
         return"user/register";
     }
 
     @PostMapping("/register")
     public String saveUser(@ModelAttribute User user){
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
         userDao.save(user);
         return "redirect:/login";
     }
-}
+
+}// class
