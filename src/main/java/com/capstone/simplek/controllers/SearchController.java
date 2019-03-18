f47f4254575ef65e3bfd59966d504abb246169fa
AaronBoehle <boehle.aaron@gmail.com>
2019-03-18 16:09:32 -0500

    school listing displays on school-results page. database updated to accomodate.

--- src/main/java/com/capstone/simplek/controllers/SearchController.java
+++ src/main/java/com/capstone/simplek/controllers/SearchController.java
@@ -1,15 +1,15 @@
 package com.capstone.simplek.controllers;
 
 import com.capstone.simplek.Model.Children;
+import com.capstone.simplek.Model.School;
 import com.capstone.simplek.Model.User;
 import com.capstone.simplek.Repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestAttribute;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class SearchController {
@@ -27,10 +27,12 @@ public class SearchController {
     private UserRepository userDao;
 
 //     requests that interact with our Dao Factory
-//    @GetMapping("search")
-//    public String all(Model model) {
-//
-//    }
+    @GetMapping("/school-results")
+    public String all(Model model) {
+        List<School> schools = schoolDao.findAll();
+        model.addAttribute("schools", schools);
+        return "school-results";
+    }
     @GetMapping("/search/create")
     public String viewSearchQuery (){
         return "search";
@@ -44,15 +46,14 @@ public class SearchController {
     {
             User user = userDao.findOne(id);
             user.setAddress(address);
-
             Children child = new Children();
 
 
         return "redirect:/school-results";
     }
 
-    @GetMapping("school-results")
-    public String schoolResults () {
-        return "school-results";
-    }
+//    @GetMapping("school-results")
+//    public String schoolResults () {
+//        return "school-results";
+//    }
 }
