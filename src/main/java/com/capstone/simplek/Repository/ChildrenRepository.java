1d1a432bfc85477866acf3f2c8da6e57b9545a67
Ron Soeum <ronsoeum@gmail.com>
2019-03-20 02:52:08 -0500

    added custom query to find all children

--- src/main/java/com/capstone/simplek/Repository/ChildrenRepository.java
+++ src/main/java/com/capstone/simplek/Repository/ChildrenRepository.java
@@ -1,10 +1,17 @@
 package com.capstone.simplek.Repository;
-
 import com.capstone.simplek.Model.Children;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 // comes with built in findAll, findOne, save, and delete methods
 // JpaRepository implements the same functionality as the CrudRepository
-public interface ChildrenRepository extends JpaRepository<Children, String> {
+@Repository
+public interface ChildrenRepository extends JpaRepository<Children, Long> {
+
+    @Query(value = "SELECT * FROM children WHERE user_id = :user_id", nativeQuery = true)
+    List<Children> getAllChildren(@Param("user_id") long user_id);
+
 }
