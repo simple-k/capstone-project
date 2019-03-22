2efeeb6afc047ca3e0a174d8a7a1cfd9e3967a21
Ron Soeum <ronsoeum@gmail.com>
2019-03-22 05:40:59 -0500

    updated search form and results with bug fixes and styling

--- src/main/java/com/capstone/simplek/controllers/SearchController.java
+++ src/main/java/com/capstone/simplek/controllers/SearchController.java
@@ -1,7 +1,6 @@
 package com.capstone.simplek.controllers;
 import com.capstone.simplek.Model.Children;
 import com.capstone.simplek.Model.Query;
-import com.capstone.simplek.Model.School;
 import com.capstone.simplek.Model.User;
 import com.capstone.simplek.Repository.*;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,7 +11,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpSession;
-import java.util.List;
 
 @Controller
 public class SearchController {
@@ -21,19 +19,25 @@ public class SearchController {
     @Autowired
     private ChildrenRepository childrenDao;
     @Autowired
-    private DistrictRepository districtDao;
-    @Autowired
     private SchoolRepository schoolDao;
-    @Autowired
-    private ServiceRepository serviceDao;
     @Autowired
     private UserRepository userDao;
 
      // requests that interact with our Dao Factory
     @GetMapping("/search")
     public String all(Model model) {
-        List<School> schools = schoolDao.findAll();
-        model.addAttribute("schools", schools);
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        boolean isAuthenticated;
+        if (authentication != null) {
+            isAuthenticated = authentication instanceof AnonymousAuthenticationToken ? false
+                    : authentication.isAuthenticated();
+            if (isAuthenticated) {
+                User loggedInUser = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+                User copyUser = userDao.findOne(loggedInUser.getId());
+                model.addAttribute("user", copyUser);
+            }
+        }
+        model.addAttribute("schools", schoolDao.findAll());
         return "search/index";
     }
 
@@ -49,7 +53,7 @@ public class SearchController {
                 User copyUser = userDao.findOne(loggedInUser.getId());
                 Children copyChild = childrenDao.getFirstChild(copyUser.getId());
                 model.addAttribute("user", copyUser);
-                model.addAttribute("query", new Query());
+                model.addAttribute("queries", new Query());
                 if (copyChild != null) {
                     model.addAttribute("child", copyChild);
                 } else {
@@ -72,46 +76,40 @@ public class SearchController {
 
         boolean userInputHasErrors = user.getuFirstName().isEmpty()
                 || user.getuLastName().isEmpty()
-                || user.getEmail().isEmpty()
                 || user.getAddress().isEmpty()
-//                || user.getZipCode().isEmpty()
                 || user.getPhoneNumber().isEmpty();
+//                || user.getZipCode().isEmpty()
 
-        // check if inputs are empty
+        // check if user inputs are empty
         if (userInputHasErrors) {
             session.setAttribute("inputErrors", "Invalid inputs!");
             return "redirect:/search/query";
-        // check if email matches current user email in the database
-        } else if (user.getEmail().equals(copyUser.getEmail())){
-            //update user with same email input as database
-            copyUser.setuFirstName(user.getuFirstName());
-            copyUser.setuLastName(user.getuLastName());
-            copyUser.setPhoneNumber(user.getPhoneNumber());
-            copyUser.setAddress(user.getAddress());
-            copyUser.setZipCode(user.getZipCode());
-            userDao.save(copyUser);
-        } else {
-        // check if email already exist in the users table
-            User checkEmail = userDao.findByEmail(user.getEmail());
-            if (checkEmail != null) {
-                session.setAttribute("inputErrors", "Email already exist!");
-                return "redirect:/search/query";
-            }
-        }// email check
+        }
 
         // update user
         copyUser.setuFirstName(user.getuFirstName());
         copyUser.setuLastName(user.getuLastName());
-        copyUser.setEmail(user.getEmail());
         copyUser.setPhoneNumber(user.getPhoneNumber());
         copyUser.setAddress(user.getAddress());
-        copyUser.setZipCode(user.getZipCode());
+//        copyUser.setZipCode(user.getZipCode());
         userDao.save(copyUser);
 
-        // save/update child
+        boolean childInputHasErrors = child.getFirstName().isEmpty()
+                || child.getLastName().isEmpty()
+                || child.getDob() == null
+                || Character.isSpaceChar(child.getGender());
+
+        // check if child inputs are empty
+        if (childInputHasErrors) {
+            session.setAttribute("inputErrors", "Invalid inputs!");
+            return "redirect:/search/query";
+        }
+
+        // check if child exist in database
         if (copyChild != null
                 && copyChild.getFirstName().equals(child.getFirstName())
                 && copyChild.getLastName().equals(child.getLastName())) {
+            // update child info
             copyChild.setUser(copyUser);
             copyChild.setFirstName(child.getFirstName());
             copyChild.setLastName(child.getLastName());
@@ -121,6 +119,8 @@ public class SearchController {
             // set session attribute for child
             session.setAttribute("child", copyChild);
         } else {
+            // if child does not exist in database
+            // save new child
             child.setUser(copyUser);
             child.setFirstName(child.getFirstName());
             child.setLastName(child.getLastName());
@@ -132,9 +132,8 @@ public class SearchController {
         }// save/update child
 
         // set session attribute for search results
-        session.setAttribute("query", query);
-
-        return "redirect:/search/search";
+        session.setAttribute("queries", query);
+        return "redirect:/search";
 
     }// createSearchQuery
 
