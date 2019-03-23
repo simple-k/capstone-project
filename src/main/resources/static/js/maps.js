b58a1ee30f7a44dc32dba74bfcb8fe6eda3d0ed7
Ron Soeum <ronsoeum@gmail.com>
2019-03-23 00:22:56 -0500

    added ajax request for seleted school, and renders div with school info on click

--- src/main/resources/static/js/maps.js
+++ src/main/resources/static/js/maps.js
@@ -21,8 +21,44 @@ map.data.loadGeoJson(
 
 var geocoder = new google.maps.Geocoder();
 
-function locate(address) {
-    alert('thisworks!');
+function locate(address, id) {
+    console.log('thisworks!');
+
+    // show div with selected school
+    $("#selected_school").show("slow");
+    // CSRF token
+    var token = $("meta[name='_csrf']").attr("content");
+    var header = $("meta[name='_csrf_header']").attr("content");
+    // get selected school from database and return JSON
+    jQuery.ajax({
+        url: '/api/school/selected',
+        type: 'GET',
+        dataType: 'json',
+        data: {"schoolId": id},
+        cache:false,
+        beforeSend: function(xhr) {
+            xhr.setRequestHeader(header, token);
+        },
+            success:function(school){
+                // console.log(school);
+                $("#school").remove();
+                $('#selected_school').append("<div id='school'>" +
+                    "<h3>"+school.schoolName+"</h3>" +
+                    "<p>"+school.streetAddress+"</p>" +
+                    "<p>San Antonio, TX, <span>"+school.zipCode+"</span></p>" +
+                    "<p>Grades: PK-<span>"+school.highGrade+"</span></p>" +
+                    "<p>Total students enrolled: <span>"+school.students+"</span></p>" +
+                    "<p>Total teachers: <span>"+school.teachers+"</span></p>" +
+                    "<p>Students per teacher: <span>"+school.studentTeacherRatio+"</span></p>" +
+                    "<p>District: </p></div>");
+        },
+        error:function(jqXhr, textStatus, errorThrown){
+            console.log(jqXhr);
+            console.log(textStatus);
+            console.log(errorThrown);
+        }
+    });
+
     geocoder.geocode({"address": address}, function (results, status) {
 
         // Check for a successful result
@@ -36,7 +72,7 @@ function locate(address) {
             });
         } else {
 
-            // Show an error message with the status if our request fails
+            // Show an error Message with the status if our request fails
             alert("Geocoding was not successful - STATUS: " + status);
         }
     });
