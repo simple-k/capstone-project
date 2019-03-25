7920b49789c4de89347e6702604c0e8d33cbfb19
AaronBoehle <boehle.aaron@gmail.com>
2019-03-25 11:58:54 -0500

    adding styling options to google maps polygons.

--- src/main/resources/static/js/maps.js
+++ src/main/resources/static/js/maps.js
@@ -17,8 +17,44 @@ map = new google.maps.Map(document.getElementById('map-canvas'), {
 // NOTE: This uses cross-domain XHR, and may not work on older browsers.
 map.data.loadGeoJson(
     '../Json/San_Antonio_Districts.geojson');
+// Color each letter gray. Change the color when the isColorful property
+// is set to true.
+
+// STYLING OPTIONS FOR GEOJSON
+map.data.setStyle({strokeWeight: 1});
+// map.data.setStyle(function(feature) {
+//     var color = 'gray';
+//     if (feature.getProperty('isColorful')) {
+//         color = feature.getProperty('color');
+//     }
+//     return /** @type {!google.maps.Data.StyleOptions} */({
+//         fillColor: color,
+//         strokeColor: color,
+//         strokeWeight: 2
+//     });
+// });
+//
+// // When the user clicks, set 'isColorful', changing the color of the letters.
+// map.data.addListener('click', function(event) {
+//     event.feature.setProperty('isColorful', true);
+// });
+//
+// // When the user hovers, tempt them to click by outlining the letters.
+// // Call revertStyle() to remove all overrides. This will use the style rules
+// // defined in the function passed to setStyle()
+// map.data.addListener('mouseover', function(event) {
+//     map.data.revertStyle();
+//     map.data.overrideStyle(event.feature, {strokeWeight: 8});
+// });
+//
+// map.data.addListener('mouseout', function(event) {
+//     map.data.revertStyle();
+// });
+
+
 
 var geocoder = new google.maps.Geocoder();
+
 
 function locate(address, id) {
     console.log('thisworks!');
